5df585701ebe2178f523678c197602f3dcad45fb
keshavsharma7009 <keshav.sharma2021@vitstudent.ac.in>
2025-08-03 21:29:48 +0530

    Add files via upload

---
+++ 2_Aug_Lab_work/pizza.py
@@ -0,0 +1,6 @@
+def myfunc(name, lastname):
+    for n in name:
+        for m in lastname:
+            print(f'Hi my name is {n} {m}.')
+
+myfunc(name=['keshav','Jaish','Shriharsh'],lastname=['sharma','arun','kotecha'])
